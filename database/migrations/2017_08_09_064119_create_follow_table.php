4876d681957efd84117c2a1f5ee62ba861bd0fc3
hisaki0616 <hisaki0616@gmail.com>
2017-08-09 15:44:49 +0900

    create new migation file

---
+++ database/migrations/2017_08_09_064119_create_follow_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFollowTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follow', function (Blueprint $table) {
+            $table->integer('follower_id');
+            $table->integer('followee_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follow');
+    }
+}
