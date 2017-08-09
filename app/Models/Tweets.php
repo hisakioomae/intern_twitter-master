88c496729c7db906ec8e33c76c4d76c1d6952df0
hisaki0616 <hisaki0616@gmail.com>
2017-08-09 12:16:03 +0900

    create Tweets model

--- app/Models/Tweets.php
+++ app/Models/Tweets.php
@@ -5,3 +5,32 @@
  * Date: 2017/08/09
  * Time: 12:01
  */
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+
+class Tweets extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * 複数代入を行う属性
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'body',
+    ];
+
+    /**
+     * 配列には含めない属性
+     *
+     * @var array
+     */
+    protected $hidden = [
+        // 無し
+    ];
+}
